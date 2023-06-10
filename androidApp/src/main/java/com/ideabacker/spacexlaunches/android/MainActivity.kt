57925a5402cb95a6ab945f7571a6766cc8ea4cf6
Arthur Kazemi <bidadh@gmail.com>
2023-06-11 03:25:24 +1000

    complete Android App

--- androidApp/src/main/java/com/ideabacker/spacexlaunches/android/MainActivity.kt
+++ androidApp/src/main/java/com/ideabacker/spacexlaunches/android/MainActivity.kt
@@ -1,22 +1,75 @@
 package com.ideabacker.spacexlaunches.android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-//import com.jetbrains.handson.kmm.shared.SpaceXSDK
-//import com.jetbrains.handson.kmm.shared.cache.DatabaseDriverFactory
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
-import androidx.core.view.isVisible
+import com.ideabacker.spacexlaunches.shared.SpaceXSDK
+import com.ideabacker.spacexlaunches.shared.cache.DatabaseDriverFactory
 import com.jetbrains.handson.androidApp.R
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var launchesRecyclerView: RecyclerView
+  private lateinit var progressBarView: FrameLayout
+  private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+  private val launchesRvAdapter = LaunchesRvAdapter(listOf())
+  private val sdk = SpaceXSDK(DatabaseDriverFactory(this))
+
+  private val mainScope = MainScope()
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    title = "SpaceX Launches"
+    setContentView(R.layout.activity_main)
+
+    launchesRecyclerView = findViewById(R.id.launchesListRv)
+    progressBarView = findViewById(R.id.progressBar)
+    swipeRefreshLayout = findViewById(R.id.swipeContainer)
+
+    launchesRecyclerView.adapter = launchesRvAdapter
+    launchesRecyclerView.layoutManager = LinearLayoutManager(this)
+
+    swipeRefreshLayout.setOnRefreshListener {
+      swipeRefreshLayout.isRefreshing = false
+      displayLaunches(true)
+    }
+
+    displayLaunches(false)
+  }
+
+  @SuppressLint("NotifyDataSetChanged")
+  private fun displayLaunches(needReload: Boolean) {
+    progressBarView.isVisible = true
+    mainScope.launch {
+      kotlin.runCatching {
+        sdk.getLaunches(needReload)
+      }.onSuccess {
+        launchesRvAdapter.launches = it
+        launchesRvAdapter.notifyDataSetChanged()
+      }.onFailure {
+        Toast.makeText(this@MainActivity, it.localizedMessage, Toast.LENGTH_SHORT).show()
+      }
+      progressBarView.isVisible = false
+    }  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    mainScope.cancel()
+  }
+}
+
+/*
+class MainActivity2 : AppCompatActivity() {
     private val mainScope = MainScope()
 
     private lateinit var launchesRecyclerView: RecyclerView
@@ -67,3 +120,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+*/
